d8174323ef5a6ce599172c25fc1c0e348f7d6b98
Jonilson da Silva Leles <jsleles@gmail.com>
2025-06-22 12:14:46 -0300

    Consulta simples com query methods

--- src/main/java/com/nelioalves/workshopmongo/service/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.workshopmongo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,7 @@ public class PostService {
 		return post.orElseThrow( ()-> new ObjectNotFoundException("Objeto não encontrato"));
 	}
 	
+	public List<Post> findByTitle (String text) {
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
 }
